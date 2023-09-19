fb8c3e8854ba6c0eda5bdd8bc0cda1e824bd98b0
putintsevaliza <karezinaliza@yandex.ru>
2023-09-19 11:03:46 +0300

    Вывод точки на экран

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,2 +1,16 @@
-﻿    // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет, мир");
+﻿Console.SetWindowSize(40, 30);
+Console.SetBufferSize(40, 30);
+int x1 = 2;
+int y1 = 3;
+char c1 = '*';
+Drow(x1, y1, c1);
+
+
+Console.ReadLine();
+
+static void Drow(int x, int y, char c)
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(c);
+
+}
