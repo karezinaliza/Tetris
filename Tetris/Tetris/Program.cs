77c46f9422df2955d7d492a27920cb5fac1ce2d4
putintsevaliza <karezinaliza@yandex.ru>
2023-09-19 11:08:52 +0300

    Drow->Draw

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -3,12 +3,12 @@ Console.SetBufferSize(40, 30);
 int x1 = 2;
 int y1 = 3;
 char c1 = '*';
-Drow(x1, y1, c1);
+Draw(x1, y1, c1);
 
 
 Console.ReadLine();
 
-static void Drow(int x, int y, char c)
+static void Draw(int x, int y, char c)
 {
     Console.SetCursorPosition(x, y);
     Console.Write(c);
